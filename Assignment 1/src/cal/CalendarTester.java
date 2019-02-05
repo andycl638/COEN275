db78f2ff85f1f5cc9641830846289a0417006e73
Andy <andy@Andys-MacBook-Pro-3.local>
2019-02-04 17:22:36 -0800

    first upload

---
+++ Assignment 1/src/cal/CalendarTester.java
@@ -0,0 +1,11 @@
+package cal;
+
+public class CalendarTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar1 cal = new Calendar1();
+		cal.createCalendar();
+	}
+
+}
